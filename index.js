004e5e95493267c3de08ee61839cb230c3620f80
mmegarba <micaeljohnmegarbane@gmail.com>
2018-10-10 21:00:39 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+function validateEmail()
+{
+  var email = document.getElementById("email").value;
+  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(email);
+}
+
+
+
+
+
+
+function reset(){
+
+  document.getElementById("myForm").reset();
+
+}
+
+
+
+function validation(){
+
+var email = document.getElementById("email").value;
+var emailConfirmation = document.getElementById("emailConfirmation").value;
+
+
+
+if(email != emailConfirmation){
+
+
+alert("Emails Do not Match!");
+
+}
+
+}
